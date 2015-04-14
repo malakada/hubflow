8a8181ea4734b901845a0658b2cb44fe8cfa5338
Melissa Noelle <madamei@mojolingo.com>
2015-04-14 01:35:33 -0400

    Hooray, loading in multiple issues!

--- react/react.js
+++ react/react.js
@@ -1,17 +1,62 @@
-var Issue = React.createClass({
+var IssueTable = React.createClass({
+  render: function() {
+    var rows = [];
+    this.props.issues.forEach(function(issue) {
+      rows.push(<IssueRow title={issue.title} body={issue.body} />);
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  },
+});
+
+var IssueRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.title}</td>
+        <td>{this.props.body}</td>
+      </tr>
+    );
+  },
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Show issues marked as closed too.
+        </p>
+      </form>
+    );
+  },
+});
+
+var FilterableIssueTable = React.createClass({
   getInitialState: function() {
     return {
-      title: '',
-      body: '',
+      issues: [],
     };
   },
   componentDidMount: function() {
     $.get(this.props.source, function(result) {
-      var lastIssue = result[0];
       if (this.isMounted()) {
         this.setState({
-          title: lastIssue.title,
-          body: lastIssue.body,
+          issues: result,
         });
       }
     }.bind(this));
@@ -19,13 +64,14 @@ var Issue = React.createClass({
   render: function() {
     return (
       <div>
-        {this.state.title} - {this.state.body}
+        <SearchBar />
+        <IssueTable issues={this.state.issues} />
       </div>
     );
-  }
+  },
 });
 
 React.render(
-  <Issue source="https://api.github.com/repos/andrewrk/groovebasin/issues" />,
+  <FilterableIssueTable source="https://api.github.com/repos/andrewrk/groovebasin/issues" />,
   document.getElementById('content')
 );
